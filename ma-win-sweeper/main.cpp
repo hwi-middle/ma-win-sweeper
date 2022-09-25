1faf4ef0618133523d984b6948b831333364c7b9
Ju Hwijung <gnlwnd5084@gmail.com>
2022-09-25 19:25:12 +0900

    게임오버 구현

--- ma-win-sweeper/main.cpp
+++ ma-win-sweeper/main.cpp
@@ -51,10 +51,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	HDC hdc;
 	PAINTSTRUCT ps;
 
+	static bool bIsGameOver = false;
 	static Board board(BOARD_SIZE, MINE_NUM);
 	static RECT rects[BOARD_SIZE][BOARD_SIZE];
 	static std::pair<int, int> leftClickedRect = { -1,-1 };
 	static std::pair<int, int> rightClickedRect = { -1,-1 };
+	static int remainMine = MINE_NUM;
 
 	switch (iMessage)
 	{
@@ -94,15 +96,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		markBrush = CreateHatchBrush(HS_DIAGCROSS, RGB(255, 0, 0));
 		mineBrush = CreateSolidBrush(RGB(255, 0, 0));
 		constexpr std::pair<int, int> NOT_CLICKED = { -1,-1 };
-		LPCTSTR description = TEXT("Press 'R' to Restart");
-		RECT clientRect;
-		GetClientRect(hWnd, &clientRect);
-		RECT descriptionRect = { 0, 20, clientRect.right, 50 };
-		DrawText(hdc, description, lstrlen(description), &descriptionRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
 
 		const int leftClickedR = leftClickedRect.first;
 		const int leftClickedC = leftClickedRect.second;
-		if (leftClickedRect != NOT_CLICKED && board.GetCellState(leftClickedR, leftClickedC) == CellState::Closed)
+		if (leftClickedRect != NOT_CLICKED && board.GetCellState(leftClickedR, leftClickedC) == CellState::Closed && !bIsGameOver)
 		{
 			constexpr int DIRECTION_NUM = 8;
 			std::queue<std::pair<int, int>> bfsQueue;
@@ -111,84 +108,136 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 			if (board.GetCellMineNum(leftClickedR, leftClickedC) == 0)
 			{
 				bfsQueue.push({ leftClickedR , leftClickedC });
+				constexpr int deltaR[DIRECTION_NUM] = { 1,0,-1,0,1,-1,-1,1 };
+				constexpr int deltaC[DIRECTION_NUM] = { 0,1,0,-1,-1,1,-1,1 };
+
+				while (!bfsQueue.empty())
+				{
+					std::pair<int, int> cur = bfsQueue.front();
+					bfsQueue.pop();
+
+					const int curR = cur.first;
+					const int curC = cur.second;
+
+					if (board.GetCellMineNum(curR, curC) != 0)
+					{
+						board.SetCellState(curR, curC, CellState::Opened);
+						continue;
+					}
+
+					for (int dir = 0; dir < DIRECTION_NUM; dir++)
+					{
+						const int newR = curR + deltaR[dir];
+						const int newC = curC + deltaC[dir];
+						if (newR < 0 || newR >= BOARD_SIZE || newC < 0 || newC >= BOARD_SIZE) continue;
+						if (board.GetCellState(newR, newC) != CellState::Closed) continue;
+
+						board.SetCellState(newR, newC, CellState::Opened);
+						bfsQueue.push({ newR,newC });
+					}
+				}
 			}
-
-			constexpr int deltaR[DIRECTION_NUM] = { 1,0,-1,0,1,-1,-1,1 };
-			constexpr int deltaC[DIRECTION_NUM] = { 0,1,0,-1,-1,1,-1,1 };
-
-			while (!bfsQueue.empty())
+			else if (board.GetCellMineNum(leftClickedR, leftClickedC) == MINE)
 			{
-				std::pair<int, int> cur = bfsQueue.front();
-				bfsQueue.pop();
-
-				const int curR = cur.first;
-				const int curC = cur.second;
-
-				if (board.GetCellMineNum(curR, curC) != 0)
-				{
-					board.SetCellState(curR, curC, CellState::Opened);
-					continue;
-				}
-
-				for (int dir = 0; dir < DIRECTION_NUM; dir++)
-				{
-					const int newR = curR + deltaR[dir];
-					const int newC = curC + deltaC[dir];
-					if (newR < 0 || newR >= BOARD_SIZE || newC < 0 || newC >= BOARD_SIZE) continue;
-					if (board.GetCellState(newR, newC) != CellState::Closed) continue;
-
-					board.SetCellState(newR, newC, CellState::Opened);
-					bfsQueue.push({ newR,newC });
-				}
+				bIsGameOver = true;
 			}
 		}
 
-		leftClickedRect = NOT_CLICKED;
-
 		const int rightClickedR = rightClickedRect.first;
 		const int rightClickedC = rightClickedRect.second;
-		if (rightClickedRect != NOT_CLICKED)
+		if (rightClickedRect != NOT_CLICKED && !bIsGameOver)
 		{
 			if (board.GetCellState(rightClickedR, rightClickedC) == CellState::Closed)
 			{
 				board.SetCellState(rightClickedR, rightClickedC, CellState::MarkedAsMine);
+				remainMine--;
 			}
 			else if (board.GetCellState(rightClickedR, rightClickedC) == CellState::MarkedAsMine)
 			{
 				board.SetCellState(rightClickedR, rightClickedC, CellState::Closed);
+				remainMine++;
 			}
 		}
-		// 우클릭 신호가 들어왔지만 조건에 맞지 않았을 경우도 있으므로 if문 밖에서 대입
-		rightClickedRect = NOT_CLICKED;
 
-
-		for (int r = 0; r < BOARD_SIZE; r++)
+		if (bIsGameOver)
 		{
-			for (int c = 0; c < BOARD_SIZE; c++)
+			for (int r = 0; r < BOARD_SIZE; r++)
 			{
-				if (board.GetCellState(r, c) == CellState::Opened)
+				for (int c = 0; c < BOARD_SIZE; c++)
 				{
-					defaultBrush = (HBRUSH)SelectObject(hdc, openedBrush);
-					Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
-					int cellMineNum = board.GetCellMineNum(r, c);
-					std::wstring cellMineNumText = std::to_wstring(cellMineNum);
-					DrawText(hdc, cellMineNumText.c_str(), cellMineNumText.length(), &rects[r][c], DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-					SelectObject(hdc, defaultBrush);
+					if (board.GetCellMineNum(r, c) == MINE)
+					{
+						defaultBrush = (HBRUSH)SelectObject(hdc, mineBrush);
+						Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+						SelectObject(hdc, defaultBrush);
+					}
+					else
+					{
+						if (board.GetCellState(r, c) == CellState::MarkedAsMine)
+						{
+							defaultBrush = (HBRUSH)SelectObject(hdc, markBrush);
+							Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+							SelectObject(hdc, defaultBrush);
+						}
+						else
+						{
+							defaultBrush = (HBRUSH)SelectObject(hdc, openedBrush);
+							Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+							int cellMineNum = board.GetCellMineNum(r, c);
+							std::wstring cellMineNumText = std::to_wstring(cellMineNum);
+							DrawText(hdc, cellMineNumText.c_str(), cellMineNumText.length(), &rects[r][c], DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+							SelectObject(hdc, defaultBrush);
+						}
+					}
 				}
-				else if (board.GetCellState(r, c) == CellState::MarkedAsMine)
+			}
+		}
+		else
+		{
+			for (int r = 0; r < BOARD_SIZE; r++)
+			{
+				for (int c = 0; c < BOARD_SIZE; c++)
 				{
-					defaultBrush = (HBRUSH)SelectObject(hdc, markBrush);
-					Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
-					DrawText(hdc, TEXT("!"), 1, &rects[r][c], DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-					SelectObject(hdc, defaultBrush);
-				}
-				else
-				{
-					Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+					if (board.GetCellState(r, c) == CellState::Opened)
+					{
+						defaultBrush = (HBRUSH)SelectObject(hdc, openedBrush);
+						Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+						int cellMineNum = board.GetCellMineNum(r, c);
+						std::wstring cellMineNumText = std::to_wstring(cellMineNum);
+						DrawText(hdc, cellMineNumText.c_str(), cellMineNumText.length(), &rects[r][c], DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+						SelectObject(hdc, defaultBrush);
+					}
+					else if (board.GetCellState(r, c) == CellState::MarkedAsMine)
+					{
+						defaultBrush = (HBRUSH)SelectObject(hdc, markBrush);
+						Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+						DrawText(hdc, TEXT("●"), lstrlen(TEXT("●")), &rects[r][c], DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+						SelectObject(hdc, defaultBrush);
+					}
+					else
+					{
+						Rectangle(hdc, rects[r][c].left, rects[r][c].top, rects[r][c].right, rects[r][c].bottom);
+					}
 				}
 			}
 		}
 
+
+
+		LPCTSTR description = TEXT("Press 'R' to Restart");
+
+		RECT clientRect;
+		GetClientRect(hWnd, &clientRect);
+
+		RECT descriptionRect = { 0, 20, clientRect.right, 50 };
+		DrawText(hdc, description, lstrlen(description), &descriptionRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		RECT mineNumRect = { 400, 20, clientRect.right, 50 };
+		std::wstring remainMineText = std::to_wstring(remainMine);
+		DrawText(hdc, remainMineText.c_str(), remainMineText.length(), &mineNumRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		leftClickedRect = NOT_CLICKED;
+		rightClickedRect = NOT_CLICKED;
 		DeleteObject(markBrush);
 		DeleteObject(mineBrush);
 		EndPaint(hWnd, &ps);
@@ -251,6 +300,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 		{
 		case 'r':
 			board.InitMap();
+			bIsGameOver = false;
+			remainMine = MINE_NUM;
 			InvalidateRect(hWnd, NULL, TRUE);
 			break;
 		}
